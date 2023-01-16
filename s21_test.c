e45e7eed72c8f3ccfb099fc0938702bdba604edb
Diana Daikon <dianadai@mi-i2.msk.21-school.ru>
2023-01-16 18:28:43 +0300

    Comm5_Tests_for_abs_and_fmod

--- s21_test.c
+++ s21_test.c
@@ -2,34 +2,87 @@
 #include <check.h>
 #include <math.h>
 #include "s21_math.h"
+START_TEST(abs_of_positive_int)
+{
+    int a = rand();
+    ck_assert_int_eq(s21_abs(a*a),abs(a*a));
+}
+END_TEST
+START_TEST(abs_of_negative_int)
+{
+    int a = rand();
+    ck_assert_int_eq(s21_abs(-a*a),abs(-a*a));
+}
+END_TEST
+START_TEST(abs_of_zero)
+{
+    ck_assert_int_eq(s21_abs(0),abs(0));
+}
+END_TEST
+START_TEST(fabs_of_positive_double)
+{
+    int min = 1;
+    int max = 10000;
+    double a = (double) rand() / RAND_MAX * (max - min) + min;
+    ck_assert_double_eq_tol(s21_fabs(a),fabs(a),1e-6);
+}
+END_TEST
+START_TEST(fabs_of_negative_double)
+{
+    int min = -10000;
+    int max = -1;
+    double a = (double) rand() / RAND_MAX * (max - min) + min;
+    ck_assert_double_eq_tol(s21_fabs(a),fabs(a),1e-6);
+}
+END_TEST
+START_TEST(fmod_with_numbers_more_then_zero)
+{
+    int min = 1;
+    int max = 10000;
+    double a = (double) rand() / RAND_MAX * (max - min) + min;
+    double b = (double) rand() / RAND_MAX * (max - min) + min;
+    ck_assert_double_eq_tol(s21_fmod(a,b),fmod(a,b),1e-6);
+}
+END_TEST
 /*
-int s21_abs(int x) {
-    if (x < 0)
-        x = -x;
-    return x;
-}*/
-START_TEST(abs_of_positive_number)
+START_TEST(fmod_with_numbers_less_then_zero)
 {
-    int a = 1;
-    ck_assert_int_eq(s21_abs(a),abs(a));
+    int min = -10000;
+    int max = -1;
+    double a = (double) rand() / RAND_MAX * (max - min) + min;
+    double b = (double) rand() / RAND_MAX * (max - min) + min;
+    ck_assert_double_eq_tol(s21_fmod(a,b),fmod(a,b),1e-6);
 }
 END_TEST
-START_TEST(abs_of_negative_number)
+START_TEST(fmod_with_one_numbers_less_and_one_more_then_zero)
 {
-    int a = -1;
-    ck_assert_int_eq(s21_abs(a),abs(a));
+    double a = 9.;
+    double b = -6.;
+    ck_assert_double_eq_tol(s21_fmod(a,b),fmod(a,b),1e-6);
 }
 END_TEST
+*/
 
 int main() {
-    Suite *s1 = suite_create("Tests");
-    TCase *tc_1 = tcase_create("Tests_for_math");
+    Suite *s1 = suite_create("Tests_for_math");
+    
+    TCase *tc_abs = tcase_create("Tests_for_abs");
+    TCase *tc_fmod = tcase_create("Tests_for_fmod");
     SRunner *sr = srunner_create(s1);
     
-    suite_add_tcase(s1, tc_1);
+    suite_add_tcase(s1, tc_abs);
+    suite_add_tcase(s1, tc_fmod);
     
-    tcase_add_test(tc_1, abs_of_positive_number);
-    tcase_add_test(tc_1, abs_of_negative_number);
+    tcase_add_test(tc_abs, abs_of_positive_int);
+    tcase_add_test(tc_abs, abs_of_negative_int);
+    tcase_add_test(tc_abs, fabs_of_positive_double);
+    tcase_add_test(tc_abs, fabs_of_negative_double);
+    tcase_add_test(tc_abs, abs_of_zero);
+    
+    tcase_add_test(tc_fmod, fmod_with_numbers_more_then_zero);
+//  tcase_add_test(tc_1, fmod_with_numbers_less_then_zero);
+//  tcase_add_test(tc_1, fmod_with_one_numbers_less_and_one_more_then_zero);
+
     
     srunner_run_all(sr, CK_ENV);
     int a = srunner_ntests_failed(sr);
