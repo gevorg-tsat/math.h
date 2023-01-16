a2f2538237ec336f5493049565d5d7ec5879d9af
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-01-16 22:56:17 +0300

    merged and pow fixed

--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 #include <math.h>
 
 int main() {
+    printf("%f", pow(S21_PLUS_INF, 0));
     return 0;
 }
--- s21_math.c
+++ s21_math.c
@@ -103,8 +103,24 @@ long double s21_log(double x) {
 
 long double s21_pow(double base, double exp){
     long double res = 1;
-    
-    if(base < 0.){
+    if (exp == 0)
+        res = 1;
+    else if (base == 0) {
+        if(S21_isnan(exp))
+            res = S21_NAN;
+        else
+            res = 0;
+    }
+    else if (base == 1) {
+        res = 1;
+    }
+    else if (S21_isinf(exp)) {
+        if ((exp > 0 && s21_fabs(base) > 1) || (exp < 0 && s21_fabs(base) < 1))
+            res = S21_PLUS_INF;
+        else
+            res = 0;
+    }
+    else if(base < 0.){
         if (exp == s21_floor(exp)) {
             if (exp > 0)
                 for (long double i = 1; i <= exp; i++, res *= base);
@@ -118,10 +134,10 @@ long double s21_pow(double base, double exp){
     else {
         res = s21_exp(exp * s21_log(base));
     }
-    if (res < - S21_MAX + 1 && !(S21_isnan(res)))
+    if (S21_isinf(res) && res < 0)
         res = S21_MINUS_INF;
-    else if (res > S21_MAX && !(S21_isnan(res)))
-        res = S21_MINUS_INF;
+    else if (S21_isinf(res) && res > 0)
+        res = S21_PLUS_INF;
     return res;
 }
 
