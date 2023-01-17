a5ecd6ffdc1608c143281441ac1735549af1a1b2
Trunks Loan <trunkslo@mi-l4.msk.21-school.ru>
2023-01-17 15:49:20 +0300

    final trig I guess

--- s21_math.c
+++ s21_math.c
@@ -96,7 +96,7 @@ long double s21_log(double x) {
 
 long double s21_pow(double base, double exp){
     long double res = 1;
-    
+
     if(base < 0.){
         if (exp == s21_floor(exp)) {
             if (exp > 0)
@@ -139,10 +139,11 @@ long double s21_cos(double x) {
 
 long double s21_tan(double x) {
   x = fmod(x, S21_PI);
-  long double result = S21_NAN;
-  if ((s21_fabs(x - S21_PI_2) > 1e-6) && !S21_isinf(x) && !S21_isnan(x)) {
+  long double result = 0;
+  if (((s21_fabs(s21_fabs(x) - S21_PI_2) < 1e-6) || S21_isinf(x) || S21_isnan(x)) && (x != 0))
+    result = S21_NAN;
+  else
     result = s21_sin(x) / s21_cos(x);
-  }
   return result;
 }
 
@@ -155,9 +156,14 @@ long double s21_asin(double x) {
       q = q * (x * x * (2 * i + 1) * (2 * i + 1)) / (2 * (2 * i + 3) * (i + 1));
       i++;
     }
-  } else if (s21_fabs(x) <= 1) {
+  } else if (s21_fabs(x) < 1) {
     sum = S21_PI_2 - 2 * s21_asin(s21_sqrt((1 - x) / 2));
-  } else 
+  } else if (s21_abs(x) == 1) {
+    if (x == 1)
+      sum = S21_PI_2;
+    else 
+      sum = -S21_PI_2;
+  } else
     sum = S21_NAN;
   return sum;
 }
@@ -167,9 +173,13 @@ long double s21_acos(double x) {
 }
 
 long double s21_atan(double x) {
-  long double sum = S21_PI_2;
-  if (!S21_isinf(x)) 
+  long double sum;
+  if (!S21_isinf(x))
     sum = s21_asin(x / s21_sqrt(1 + x * x));
+  else if (x == S21_MINUS_INF)
+    sum = -S21_PI_2;
+  else if (x == S21_PLUS_INF)
+    sum = S21_PI_2;
   return sum;
 }
 
